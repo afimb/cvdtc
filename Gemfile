97e80bd273dd474a927769a39ee65e12938a9d5e
Bruno Perles <bruno@atnos.com>
2016-04-27 11:59:03 +0200

    Bump to ruby 2.3.1 version

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-ruby '2.3.0'
+ruby '2.3.1'
 gem 'bundler', '~> 1.11'
 gem 'rails', '~> 4.2.6'
 gem 'sass-rails', '~> 5.0'
--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ set :keep_assets, 2
 
 # rbenv
 set :rbenv_type, :user # or :system, depends on your rbenv setup
-set :rbenv_ruby, '2.3.0'
+set :rbenv_ruby, '2.3.1'
 
 # set :format, :pretty
 set :log_level, :debug
